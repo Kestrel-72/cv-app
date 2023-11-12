03b1838f08ad0161ef08df00b4da1862433a93ea
Roman <kavoriya@gmail.com>
2023-11-12 21:57:57 +0500

    Add parts to FormMain

--- src/FormMain.jsx
+++ src/FormMain.jsx
@@ -1,5 +1,12 @@
+import FormMainPart from "./FormMainPart"
+
 export default function FormMain() {
    return (
-      <div className="form-main"></div>
+      <div className="form-main">
+         <FormMainPart label="Full Name" placeholder="John Doe"/>
+         <FormMainPart label="Job Title" placeholder="Frontend Developer"/>
+         <FormMainPart label="Email" placeholder="johndoe@protonmail.com"/>
+         <FormMainPart label="Phone" placeholder="8-800-555-35-35"/>
+      </div>
    )
 }
